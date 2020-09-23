f6f149a13539c7a365359e72cce5605c7c21319d
Claire Hendry <claire.d.hendry@gmail.com>
2020-09-23 14:32:31 -0400

    initial setup of components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
+
+import Header from './Components/Header/Header';
+import Navbar from './Components/NavBar/NavBar';
+import About from './Components/Main/About/About';
+import Contact from './Components/Main/Contact/Contact';
+import ProjectPage from './Components/Main/Projects/ProjectPage';
+import PageNotFound from './Components/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Claire's Portfolio Site
+      <Header />
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProjectPage} />
+          <Route path="/About" component={About} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/ProjectPage" component={ProjectPage} />
+          <Route component={PageNotFound} />
+        </Switch>
     </div>
   );
 }
