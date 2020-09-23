b1d492cca0a635ce219335b9e1f405b40a37566f
Claire Hendry <claire.d.hendry@gmail.com>
2020-09-23 17:17:17 -0400

    setting up basic functionality

--- src/Components/Main/Projects/Project.js
+++ src/Components/Main/Projects/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 export default class Project extends React.Component {
 
 state = {
@@ -13,6 +13,7 @@ state = {
         return (
             <div className="project">
                 <h2>{this.state.title}</h2>
+                
             </div>
         )
     }
